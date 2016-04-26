31b696beeaa5f30c7508278007647031113f4ac5
koskapan <koskapan@gmail.com>
2016-04-26 15:36:57 +0300

    Add some code for multithreading

---
+++ MultithreadGZip/GZipThread.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MultithreadGZip
+{
+    public class GZipThread : IGzipThread, IDisposable
+    {
+        ReaderWriterLockSlim _lock;
+        Thread innerThread;
+        bool disposing;
+
+        public GZipThread(ReaderWriterLockSlim rwLock)
+        {
+            _lock = rwLock;
+            innerThread = new Thread(Start);
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            innerThread.Abort();
+            _lock.ExitWriteLock();
+            _lock.ExitReadLock();
+        }
+    }
+}
